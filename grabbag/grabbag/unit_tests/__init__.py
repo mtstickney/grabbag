44eb6942d610af3aa28fbc4a044d663aed068a38
Matthew Stickney <fiendishlinux@gmail.com>
2018-08-16 18:32:15 -0400

    Add unit test for BogonFilter middleware.

---
+++ grabbag/grabbag/unit_tests/__init__.py
@@ -0,0 +1,43 @@
+from django.test import SimpleTestCase
+from django.http import HttpRequest, HttpResponseNotFound, HttpResponse
+from django.urls import Resolver404
+
+from grabbag.middleware import BogonFilter
+
+class TestBogonFilterMiddleware(SimpleTestCase):
+    def _make_request(self, path, method='GET'):
+        request = HttpRequest()
+        request.path = path
+        request.method = method
+        return request
+
+    def test_404_returned(self):
+        def app_view(request):
+            return HttpResponseNotFound()
+
+        app_middleware = BogonFilter(app_view)
+        response = app_middleware(self._make_request('/exists/'))
+
+        self.assertEquals(response.status_code, 404)
+
+    def test_200_returned(self):
+        app_called = False
+
+        def app_view(request):
+            nonlocal app_called
+            app_called = True
+            return HttpResponse("Yep, it works")
+
+        app_middleware = BogonFilter(app_view)
+        response = app_middleware(self._make_request('/exists/'))
+
+        self.assertTrue(app_called)
+        self.assertEquals(response.status_code, 200)
+
+    def test_short_circuit_on_404(self):
+        def app_view (request):
+            self.fail("App was called ")
+
+        app_middleware = BogonFilter(app_view)
+        with self.assertRaises(Resolver404):
+            response = app_middleware(self._make_request('/does_not_exist/'))
