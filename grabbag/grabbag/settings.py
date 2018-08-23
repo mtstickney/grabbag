2d7335d0a6c1e9eafc4c147bdc18b936b3c97e53
Matthew Stickney <fiendishlinux@gmail.com>
2018-08-23 01:02:02 -0400

    Fix database transaction setting.
    Because it helps to have the setting in the right place.

--- grabbag/grabbag/settings.py
+++ grabbag/grabbag/settings.py
@@ -72,11 +72,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ATOMIC_REQUESTS': True,
     }
 }
 
-
-ATOMIC_REQUESTS=True
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
